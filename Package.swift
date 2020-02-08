83166e1cbe50469fdc32dd96113d446b8ea36db2
hmhv <admin@hmhv.info>
2020-02-08 20:51:13 +0900

    add path for package target

--- Package.swift
+++ Package.swift
@@ -16,6 +16,8 @@ let package = Package(
     targets: [
         .target(
             name: "SoftUIView",
-            dependencies: []),
+            dependencies: []
+            path: "Sources")
+),
     ]
 )
