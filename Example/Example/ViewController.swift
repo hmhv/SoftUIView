f8c25dba7081f6aa26c4779b4a9169d919ed4e42
hmhv <admin@hmhv.info>
2020-02-08 10:26:13 +0900

    add soft ui view and example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,148 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by MYUNGHOON HONG on 2020/02/08.
+//  Copyright © 2020 hmhv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleView: SoftUIView!
+    @IBOutlet weak var subtitleView: SoftUIView!
+
+    @IBOutlet weak var backwardView: SoftUIView!
+    @IBOutlet weak var playView: SoftUIView!
+    @IBOutlet weak var forwardView: SoftUIView!
+
+    @IBOutlet weak var actionView: SoftUIView!
+    var actionLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1)
+
+        setupTitleView()
+        setupSubtitleView()
+
+        setupBackwardView()
+        setupForwardView()
+        setupPlayView()
+
+        setupActionView()
+    }
+}
+
+extension ViewController {
+
+    func setupTitleView() {
+        titleView.type = .normal
+
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .darkText
+        label.font = UIFont.init(name: "AvenirNext-Medium", size: 20)
+
+        label.text = "Soft UI View"
+
+        titleView.setContentView(label)
+        label.centerXAnchor.constraint(equalTo: titleView.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: titleView.centerYAnchor).isActive = true
+    }
+
+    func setupSubtitleView() {
+        subtitleView.type = .normal
+        subtitleView.isSelected = true
+
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.init(name: "AvenirNext-Bold", size: 13)
+
+        label.text = "Neumorphism"
+
+        label.textColor = .darkText
+        subtitleView.setContentView(label)
+        label.centerXAnchor.constraint(equalTo: subtitleView.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: subtitleView.centerYAnchor).isActive = true
+    }
+
+    func setupBackwardView() {
+        backwardView.addTarget(self, action: #selector(backwardTapHandler), for: .touchUpInside)
+
+        let icon = UIImage(systemName: "backward.fill")
+        let imageView = UIImageView(image: icon)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.tintColor = .darkGray
+
+        backwardView.setContentView(imageView)
+        imageView.centerXAnchor.constraint(equalTo: backwardView.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: backwardView.centerYAnchor).isActive = true
+    }
+
+    func setupForwardView() {
+        forwardView.addTarget(self, action: #selector(forwardTapHandler), for: .touchUpInside)
+
+        let icon = UIImage(systemName: "forward.fill")
+        let imageView = UIImageView(image: icon)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.tintColor = .darkGray
+
+        forwardView.setContentView(imageView)
+        imageView.centerXAnchor.constraint(equalTo: forwardView.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: forwardView.centerYAnchor).isActive = true
+    }
+
+    func setupPlayView() {
+        playView.type = .toggleButton
+        playView.addTarget(self, action: #selector(playTapHandler), for: .touchDown)
+
+        let playIcon = UIImage(systemName: "play.fill")
+        let playImageView = UIImageView(image: playIcon)
+        playImageView.translatesAutoresizingMaskIntoConstraints = false
+        playImageView.tintColor = .darkGray
+
+        let stopIcon = UIImage(systemName: "stop.fill")
+        let stopImageView = UIImageView(image: stopIcon)
+        stopImageView.translatesAutoresizingMaskIntoConstraints = false
+        stopImageView.tintColor = .darkGray
+
+        playView.setContentView(playImageView, selectedContentView: stopImageView, selectedTransform: nil)
+        playImageView.centerXAnchor.constraint(equalTo: playView.centerXAnchor).isActive = true
+        playImageView.centerYAnchor.constraint(equalTo: playView.centerYAnchor).isActive = true
+        stopImageView.centerXAnchor.constraint(equalTo: playView.centerXAnchor).isActive = true
+        stopImageView.centerYAnchor.constraint(equalTo: playView.centerYAnchor).isActive = true
+    }
+
+    func setupActionView() {
+        subtitleView.type = .normal
+
+        actionLabel = UILabel()
+        actionLabel.translatesAutoresizingMaskIntoConstraints = false
+        actionLabel.textColor = .darkText
+        actionLabel.font = UIFont.init(name: "AvenirNext-Regular", size: 16)
+
+        actionLabel.text = "Ready"
+
+        actionView.setContentView(actionLabel)
+        actionLabel.centerXAnchor.constraint(equalTo: actionView.centerXAnchor).isActive = true
+        actionLabel.centerYAnchor.constraint(equalTo: actionView.centerYAnchor).isActive = true
+    }
+
+    @objc func backwardTapHandler() {
+        actionLabel.text = "Backward"
+    }
+
+    @objc func forwardTapHandler() {
+        actionLabel.text = "Forward"
+    }
+
+    @objc func playTapHandler() {
+        actionLabel.text = playView.isSelected ? "Playing" : "Stopped"
+    }
+
+}
+
+
--- Sources/SoftUIView/SoftUIView.swift
+++ Sources/SoftUIView/SoftUIView.swift
@@ -1,3 +1,352 @@
-struct SoftUIView {
-    var text = "Hello, World!"
+//
+//  SoftUIView.swift
+//
+
+import UIKit
+
+@objc enum SoftUIViewType: Int {
+    case pushButton
+    case toggleButton
+    case normal
+}
+
+class SoftUIView: UIControl {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createSubLayers()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        createSubLayers()
+    }
+
+    func setContentView(_ contentView: UIView?,
+                        selectedContentView: UIView? = nil,
+                        selectedTransform: CGAffineTransform? = CGAffineTransform.init(scaleX: 0.95, y: 0.95)) {
+
+        resetContentView(contentView,
+                         selectedContentView: selectedContentView,
+                         selectedTransform: selectedTransform)
+    }
+
+    var type: SoftUIViewType = .pushButton {
+        didSet { updateShadowLayers() }
+    }
+
+    var mainColor: CGColor = SoftUIView.defalutMainColorColor {
+        didSet { updateMainColor() }
+    }
+
+    var darkShadowColor: CGColor = SoftUIView.defalutDarkShadowColor {
+        didSet { updateDarkShadowColor() }
+    }
+
+    var lightShadowColor: CGColor = SoftUIView.defalutLightShadowColor {
+        didSet { updateLightShadowColor() }
+    }
+
+    var shadowOffset: CGSize = SoftUIView.defalutShadowOffset {
+        didSet { updateShadowOffset() }
+    }
+
+    var shadowOpacity: Float = SoftUIView.defalutShadowOpacity {
+        didSet { updateShadowOpacity() }
+    }
+
+    var shadowRadius: CGFloat = SoftUIView.defalutShadowRadius {
+        didSet { updateShadowRadius() }
+    }
+
+    var cornerRadius: CGFloat = SoftUIView.defalutCornerRadius {
+        didSet { updateSublayersShape() }
+    }
+
+    override var bounds: CGRect {
+        didSet { updateSublayersShape() }
+    }
+
+    override var isSelected: Bool {
+        didSet {
+            updateShadowLayers()
+            updateContentView()
+        }
+    }
+
+    override var backgroundColor: UIColor? {
+        get { .clear }
+        set { }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        switch type {
+        case .pushButton:
+            isSelected = true
+        case .toggleButton:
+            isSelected = !isSelected
+        case .normal:
+            break
+        }
+        super.touchesBegan(touches, with: event)
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        switch type {
+        case .pushButton:
+            isSelected = isTracking
+        case .normal, .toggleButton:
+            break
+        }
+        super.touchesMoved(touches, with: event)
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        switch type {
+        case .pushButton:
+            isSelected = false
+        case .normal, .toggleButton:
+            break
+        }
+        super.touchesEnded(touches, with: event)
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        switch type {
+        case .pushButton:
+            isSelected = false
+        case .normal, .toggleButton:
+            break
+        }
+        super.touchesCancelled(touches, with: event)
+    }
+
+    private var backgroundLayer: CALayer!
+    private var darkOuterShadowLayer: CAShapeLayer!
+    private var lightOuterShadowLayer: CAShapeLayer!
+    private var darkInnerShadowLayer: CAShapeLayer!
+    private var lightInnerShadowLayer: CAShapeLayer!
+
+    private var contentView: UIView?
+    private var selectedContentView: UIView?
+    private var selectedTransform: CGAffineTransform?
+
+}
+
+extension SoftUIView {
+
+    static let defalutMainColorColor: CGColor = #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1)
+    static let defalutDarkShadowColor: CGColor = #colorLiteral(red: 0.8196078431, green: 0.8039215686, blue: 0.7803921569, alpha: 1)
+    static let defalutLightShadowColor: CGColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    static let defalutShadowOffset: CGSize = .init(width: 6, height: 6)
+    static let defalutShadowOpacity: Float = 1
+    static let defalutShadowRadius: CGFloat = 5
+    static let defalutCornerRadius: CGFloat = 10
+
+}
+
+private extension SoftUIView {
+
+    func createSubLayers() {
+
+        darkOuterShadowLayer = {
+            let shadowLayer = createOuterShadowLayer(shadowColor: darkShadowColor, shadowOffset: shadowOffset)
+            layer.addSublayer(shadowLayer)
+            return shadowLayer
+        }()
+
+        lightOuterShadowLayer = {
+            let shadowLayer = createOuterShadowLayer(shadowColor: lightShadowColor, shadowOffset: shadowOffset.inverse)
+            layer.addSublayer(shadowLayer)
+            return shadowLayer
+        }()
+
+        backgroundLayer = {
+            let backgroundLayer = CALayer()
+            layer.addSublayer(backgroundLayer)
+            backgroundLayer.frame = bounds
+            backgroundLayer.cornerRadius = cornerRadius
+            backgroundLayer.backgroundColor = mainColor
+            return backgroundLayer
+        }()
+
+        darkInnerShadowLayer = {
+            let shadowLayer = createInnerShadowLayer(shadowColor: darkShadowColor, shadowOffset: shadowOffset)
+            layer.addSublayer(shadowLayer)
+            shadowLayer.isHidden = true
+            return shadowLayer
+        }()
+
+        lightInnerShadowLayer = {
+            let shadowLayer = createInnerShadowLayer(shadowColor: lightShadowColor, shadowOffset: shadowOffset.inverse)
+            layer.addSublayer(shadowLayer)
+            shadowLayer.isHidden = true
+            return shadowLayer
+        }()
+
+        updateSublayersShape()
+    }
+
+    func createOuterShadowLayer(shadowColor: CGColor, shadowOffset: CGSize) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.backgroundColor = UIColor.clear.cgColor
+        layer.fillColor = mainColor
+        layer.shadowColor = shadowColor
+        layer.shadowOffset = shadowOffset
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+        return layer
+    }
+
+    func createOuterShadowPath() -> CGPath {
+        return UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
+    }
+
+    func createInnerShadowLayer(shadowColor: CGColor, shadowOffset: CGSize) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.backgroundColor = UIColor.clear.cgColor
+        layer.fillColor = mainColor
+        layer.shadowColor = shadowColor
+        layer.shadowOffset = shadowOffset
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+        layer.fillRule = .evenOdd
+        return layer
+    }
+
+    func createInnerShadowPath() -> CGPath {
+        let path = UIBezierPath(roundedRect: bounds.insetBy(dx: -100, dy: -100), cornerRadius: cornerRadius)
+        path.append(UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius))
+        return path.cgPath
+    }
+
+    func createInnerShadowMask() -> CALayer {
+        let layer = CAShapeLayer()
+        layer.path = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
+        return layer
+    }
+
+    func updateSublayersShape() {
+        backgroundLayer.frame = bounds
+        backgroundLayer.cornerRadius = cornerRadius
+
+        darkOuterShadowLayer.path = createOuterShadowPath()
+
+        lightOuterShadowLayer.path = createOuterShadowPath()
+
+        darkInnerShadowLayer.path = createInnerShadowPath()
+        darkInnerShadowLayer.mask = createInnerShadowMask()
+
+        lightInnerShadowLayer.path = createInnerShadowPath()
+        lightInnerShadowLayer.mask = createInnerShadowMask()
+    }
+
+    func resetContentView(_ contentView: UIView?,
+                          selectedContentView: UIView? = nil,
+                          selectedTransform: CGAffineTransform? = CGAffineTransform.init(scaleX: 0.95, y: 0.95)) {
+
+        self.contentView.map {
+            $0.transform = .identity
+            $0.removeFromSuperview()
+        }
+        self.selectedContentView.map { $0.removeFromSuperview() }
+
+        contentView.map {
+            $0.isUserInteractionEnabled = false
+            addSubview($0)
+        }
+        selectedContentView.map {
+            $0.isUserInteractionEnabled = false
+            addSubview($0)
+        }
+
+        self.contentView = contentView
+        self.selectedContentView = selectedContentView
+        self.selectedTransform = selectedTransform
+
+        updateContentView()
+    }
+
+    func updateContentView() {
+        if isSelected, selectedContentView != nil {
+            showSelectedContentView()
+        } else if isSelected, selectedTransform != nil {
+            showSelectedTransform()
+        } else {
+            showContentView()
+        }
+    }
+
+    func showContentView() {
+        contentView?.isHidden = false
+        contentView?.transform = .identity
+        selectedContentView?.isHidden = true
+    }
+
+    func showSelectedContentView() {
+        contentView?.isHidden = true
+        contentView?.transform = .identity
+        selectedContentView?.isHidden = false
+    }
+
+    func showSelectedTransform() {
+        contentView?.isHidden = false
+        selectedTransform.map { contentView?.transform = $0 }
+        selectedContentView?.isHidden = true
+    }
+
+    func updateMainColor() {
+        backgroundLayer.backgroundColor = mainColor
+        darkOuterShadowLayer.fillColor = mainColor
+        lightOuterShadowLayer.fillColor = mainColor
+        darkInnerShadowLayer.fillColor = mainColor
+        lightInnerShadowLayer.fillColor = mainColor
+    }
+
+    func updateDarkShadowColor() {
+        darkOuterShadowLayer.shadowColor = darkShadowColor
+        darkInnerShadowLayer.shadowColor = darkShadowColor
+    }
+
+    func updateLightShadowColor() {
+        lightOuterShadowLayer.shadowColor = lightShadowColor
+        lightInnerShadowLayer.shadowColor = lightShadowColor
+    }
+
+    func updateShadowOffset() {
+        darkOuterShadowLayer.shadowOffset = shadowOffset
+        lightOuterShadowLayer.shadowOffset = shadowOffset.inverse
+        darkInnerShadowLayer.shadowOffset = shadowOffset
+        lightInnerShadowLayer.shadowOffset = shadowOffset.inverse
+    }
+
+    func updateShadowOpacity() {
+        darkOuterShadowLayer.shadowOpacity = shadowOpacity
+        lightOuterShadowLayer.shadowOpacity = shadowOpacity
+        darkInnerShadowLayer.shadowOpacity = shadowOpacity
+        lightInnerShadowLayer.shadowOpacity = shadowOpacity
+    }
+
+    func updateShadowRadius() {
+        darkOuterShadowLayer.shadowRadius = shadowRadius
+        lightOuterShadowLayer.shadowRadius = shadowRadius
+        darkInnerShadowLayer.shadowRadius = shadowRadius
+        lightInnerShadowLayer.shadowRadius = shadowRadius
+    }
+
+    func updateShadowLayers() {
+        darkOuterShadowLayer.isHidden = isSelected
+        lightOuterShadowLayer.isHidden = isSelected
+        darkInnerShadowLayer.isHidden = !isSelected
+        lightInnerShadowLayer.isHidden = !isSelected
+    }
+
+}
+
+extension CGSize {
+
+    var inverse: CGSize {
+        .init(width: -1 * width, height: -1 * height)
+    }
+
 }
